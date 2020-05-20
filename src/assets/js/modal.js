bebadd2db2fc92637baaf13e0b96dec97044fd30
Elisabeth Farias <elisabethfarias7@gmail.com>
2020-05-20 17:27:13 -0300

    Modal

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -33,10 +33,11 @@ function onMouseOut (event) {
   // If the mouse is near the top of the window, show the popup
   if (event.clientY < 50) {
     // Remove this event listener
-    document.removeEventListener("mouseout", onMouseOut);
+    // document.removeEventListener("mouseout", onMouseOut);
 
     // Show the popup
-    showModal('exit-intent-modal')
+    // showModal('exit-intent-modal')
+    $('#exit-intent-modal').modal('show')
   }
 }
 
